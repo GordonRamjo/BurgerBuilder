79f633c687944625c2d376d49c42e077f0527803
euWna <112548635+euWna@users.noreply.github.com>
2023-05-08 04:35:12 +0900

    DataManager.cs + TrashCan.cs + Player.cs + Lobby scene
    ㅠㅠ드디어 깃데탑 실행

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Player : MonoBehaviour
+    {
+        //HamburgerIngredient ingredient;
+        public GameObject ingredient; //player가 잡고 있는 재료 컴포넌트
+        
+        public void Catch() { 
+            //ingredient = ?
+        }
+
+        public void throwAway() {
+            Destroy(this.ingredient);
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
