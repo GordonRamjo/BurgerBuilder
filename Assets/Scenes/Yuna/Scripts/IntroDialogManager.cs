d657d7f47ff1f45770ba10ad333c3c2c69c4b55a
unknown <eccyuna@gmail.com>
2023-05-10 23:25:36 +0900

    [ADD] Dialog 끝나면 Canvas hidden 처리(#5)

--- Assets/Scenes/Yuna/Scripts/IntroDialogManager.cs
+++ Assets/Scenes/Yuna/Scripts/IntroDialogManager.cs
@@ -11,10 +11,12 @@ public class IntroDialogManager : MonoBehaviour
     private string[] introDialog = new string[] { "반갑다 !\n최고의 버거 빌더가\n되고 싶다고 ?", "내가 주는\n미션을 통과하면,\n다시 생각해보겠다 !", "지금부터 딱 한 번만\n설명할테니 빠짐없이\n새겨 듣도록." };
     private string spaceDialog = "[Space] 키를 눌러 주세요";
     private int introDialogNum = 0;
+    private Canvas introUICanvas;
 
     // Start is called before the first frame update
     void Start()
     {
+        introUICanvas = GameObject.Find("IntroUICanvas").GetComponent<Canvas>();
         StartCoroutine(_typing());
     }
 
@@ -28,6 +30,9 @@ public class IntroDialogManager : MonoBehaviour
             if (introDialogNum < introDialog.Length)
             {
                 StartCoroutine(_typing());
+            } else if (introDialogNum == introDialog.Length)
+            {
+                introUICanvas.enabled = false;
             }
         }
     }
