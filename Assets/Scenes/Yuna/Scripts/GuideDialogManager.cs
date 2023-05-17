c49531acd1acd5d7a49e03cea7c8400c9a00a7f0
Guel-git <eccyuna@gmail.com>
2023-05-18 01:36:29 +0900

    [ADD] 삭제된 거 복구

--- Assets/Scenes/Yuna/Scripts/GuideDialogManager.cs
+++ Assets/Scenes/Yuna/Scripts/GuideDialogManager.cs
@@ -8,7 +8,7 @@ public class GuideDialogManager : MonoBehaviour
 {
     public TextMeshProUGUI guide;
     public RawImage image;
-    private string[] guideDialog = new string[] { "먼저 기본 버거를 만들어 볼거다", "재료통에서 바닥용 빵을 집어\n트레이에 올려라", "패티를 그릴에 올려\n5초 동안 구워라", "타버렸군! 쓰레기통에 버려라", "다시 패티를 3초 동안 굽고\n빵 위에 올려라", "잘하네, 이제 토마토를 올려라", "다음은 양상추를 올려라", "마지막이다! 빵을 올려라"};
+    private string[] guideDialog = new string[] { "First, make a basic burger", "Pick up a bottom bread from the container\nthen place it on tray", "Grill a patty for 5 seconds", "It's burnt! Throw it in the trash", "Bake a patty for 3 seconds and place on bread", "Good job. Now put a tomato on top", "Next, put lettuce on top", "Last but not least, put the bread on top" };
     public Texture2D[] images;
     private int guideDialogNum = 0;
     private Canvas guideUICanvas;
@@ -16,7 +16,7 @@ public class GuideDialogManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        guideUICanvas = GameObject.Find("Guide").GetComponent<Canvas>();
+        guideUICanvas = GameObject.Find("GuideCanvas").GetComponent<Canvas>();
     }
 
     // Update is called once per frame
@@ -30,7 +30,8 @@ public class GuideDialogManager : MonoBehaviour
             {
                 image.texture = images[guideDialogNum];
                 guide.text = guideDialog[guideDialogNum];
-            } else if (guideDialogNum == guideDialog.Length)
+            }
+            else if (guideDialogNum == guideDialog.Length)
             {
                 guideUICanvas.enabled = false;
             }
--- Assets/Scenes/Yuna/Scripts/IntroDialogManager.cs
+++ Assets/Scenes/Yuna/Scripts/IntroDialogManager.cs
@@ -8,21 +8,21 @@ public class IntroDialogManager : MonoBehaviour
 {
     public TextMeshProUGUI tx;
     public TextMeshProUGUI spacetx;
-    private string[] introDialog = new string[] { "반갑다 !\n최고의 버거 빌더가\n되고 싶다고 ?", "내가 주는\n미션을 통과하면,\n다시 생각해보겠다 !", "지금부터 딱 한 번만\n설명할테니 빠짐없이\n새겨 듣도록." };
-    private string spaceDialog = "[Space] 키를 눌러 주세요";
+    private string[] introDialog = new string[] { "So you wanna be\nthe best burger builder?", "If you pass the mission,\nI'll reconsider!", "From now on,\nI'll explain it only once,\nso listen carefully." };
+    private string spaceDialog = "Press [Space] to continue";
     private int introDialogNum = 0;
     private Canvas introUICanvas;
     private GameObject guide;
-    private GameObject gordon;
+    private GameObject chef;
 
     // Start is called before the first frame update
     void Start()
     {
         introUICanvas = GameObject.Find("IntroUICanvas").GetComponent<Canvas>();
-        guide = GameObject.Find("Guide");
-        gordon = GameObject.Find("Gordon");
+        guide = GameObject.Find("GuideCanvas");
+        chef = GameObject.Find("Chef");
         guide.SetActive(false);
-        gordon.SetActive(false);
+        chef.SetActive(false);
         StartCoroutine(_typing());
     }
 
@@ -40,7 +40,7 @@ public class IntroDialogManager : MonoBehaviour
             {
                 introUICanvas.enabled = false;
                 guide.SetActive(true);
-                gordon.SetActive(true);
+                chef.SetActive(true);
             }
         }
     }
