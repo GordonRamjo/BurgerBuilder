a2c090e8d6b3483be372a4e729b0fdfe30ce488e
YoonKyoungMin <96673951+yoongang02@users.noreply.github.com>
2023-05-21 02:43:44 +0900

    Timer UI 추가

--- Assets/Scenes/Kyoungmin/Scripts/Timer.cs
+++ Assets/Scenes/Kyoungmin/Scripts/Timer.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class Timer : MonoBehaviour
 {
     StageManager stageManager;
     public float limitTime;
+    public TMP_Text timerText;
+    int min;
+    float sec;
     // Start is called before the first frame update
 
     private void Awake()
     {
+        min = (int)limitTime / 60;
+        sec = limitTime % 60;
+        timerText.text = string.Format("{0:D2}:{1:D2}", min, (int)sec);
         stageManager = GameObject.Find("StageManager").GetComponent<StageManager>();
     }
     void Start()
@@ -25,12 +31,15 @@ public class Timer : MonoBehaviour
             if (limitTime > 0)
             {
                 limitTime -= Time.deltaTime;
-                Debug.Log("남은 시간 : " + Mathf.Round(limitTime));
+                min = (int)limitTime / 60;
+                sec = limitTime % 60;
+                timerText.text = string.Format("{0:D2}:{1:D2}", min, (int)sec);
             }
             else
             {
                 
                 stageManager.isTimeOver = true;
+                timerText.text = string.Format("00:00");
                 Debug.Log("TIME OVER");
             }
         }
